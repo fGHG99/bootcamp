01ee85886bd7df74df69082a80751ea6ed1d1d1e
fGHG99 <azkalearn10@gmail.com>
2024-12-12 15:58:28 +0700

    express static untuk menampilkan file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const Test = require('./Routes/Test');
 const MentorNote = require('./Controllers/MentorController');
 const UploadRoute = require('./Controllers/UploadController');
 const path = require("path");
+const { protect, verifyRoles, verifyStatus } = require('./Middlewares/Auth');
 
 const app = express();
 
@@ -18,14 +19,14 @@ app.use('/admin', adminRoutes);
 app.use('/trainee', traineeRoutes);
 app.use('/test', Test);
 app.use('/mentor', MentorNote);
-app.use('/uploads', UploadRoute);
+app.use('/uploads', verifyRoles('ADMIN'), UploadRoute);
 
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-console.log("Serving static files from:", path.join(__dirname, "../public/profile"));
+// console.log("Serving static files from:", path.join(__dirname, "../public/profile"));
 
 
 const PORT = process.env.PORT || 4000;
